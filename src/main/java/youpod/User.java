b89eecbcf8bbe3184803ac1ab2b76e3926160956
VSeryi <vseryi_vr13@hotmail.com>
2015-05-20 17:31:19 +0200

    asdasd

--- src/main/java/youpod/User.java
+++ src/main/java/youpod/User.java
@@ -1,9 +1,6 @@
 package youpod;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
+import java.util.ArrayList;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -19,8 +16,59 @@ public class User {
 	private String email;
 	private String password;
 	private boolean activate;
+	private ArrayList<Integer> friends;
+	private ArrayList<Integer> music;
 
 	public User() {
 	}
 
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean isActivate() {
+		return activate;
+	}
+
+	public void setActivate(boolean activate) {
+		this.activate = activate;
+	}
+
+	public Iterable<Integer> getFriends() {
+		return friends;
+	}
+
+	public void setFriends(ArrayList<Integer> friends) {
+		this.friends = friends;
+	}
+
+	public Iterable<Integer> getMusic() {
+		return music;
+	}
+
+	public boolean addMusic(Integer music) {
+		return this.music.add(music);
+	}
+	
+	
 }
