00be1d75576bb0c9796452bf663637bf9fec8c0b
flopezn11 <fernando_lopeznieto@hotmail.es>
2015-05-21 13:32:50 +0200

    no toques

--- src/main/resources/static/app/signupcontroller.js
+++ src/main/resources/static/app/signupcontroller.js
@@ -1,42 +1,18 @@
 angular.module("app").controller("SignupController", SignupController);
 
-SignupController.$inject = ["$resource","$scope", "$auth","blogmanager"];
+SignupController.$inject = ["$resource","$scope", "$auth","userService","$location"];
 
-function SignupController($resource,$scope, $auth) {
+function SignupController($resource,$scope, $auth,userService,$location) {
 
 	var vm = this;
 	
 	var users = [];
 	
 	//View model properties
-	vm.link = "";
-	vm.downloading = false;
-	vm.loading = false;
-	vm.loaded = false;
 	vm.newUser = {};
 	
-	//Controller logic
-	
-	$scope.authenticate = function(provider) {
-      		$auth.authenticate(provider);
-    	};
-	
-	//Controller actions
-
-	vm.convertLink = function() {
-		vm.downloading = true;
-		vm.loading = true;
-		var SimpleMusic = $resource('/video/:link', {
-			link : '@link'
-		});
-		vm.music = SimpleMusic.get({youtubeLink : vm.link}, function(){
-			vm.loading = true;
-			vm.loaded = true;
-		});
-	};
-	
 	vm.addUser = function (newUser) {
-		blogmanager.newUser(newUser);
+		userService.addUser(newUser);
 		vm.newUser = {};
 		$location.path("/");
 	};
