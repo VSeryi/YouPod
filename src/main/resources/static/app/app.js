30b2259a1af296b076e9c16aa7cbd59429a1c0f5
VSeryi <vseryi_vr13@hotmail.com>
2015-05-21 13:46:42 +0200

    SDSD

--- src/main/resources/static/app/app.js
+++ src/main/resources/static/app/app.js
@@ -1,10 +1,12 @@
 angular.module("app", [ "ngResource", "ngRoute","satellizer" ]).config(function($authProvider) {
 
     $authProvider.facebook({
+    	url: '/user/facebook',
       clientId: '1639304679648814'
     });
 
     $authProvider.google({
+    	url: '/user/google',
       clientId: '572360036658-qst6aur51pjsboosagukdrd5puu6cttj.apps.googleusercontent.com'
     });
     $authProvider.twitter({
